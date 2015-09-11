9b7d012038fd5182a379fcb0c369cf08c710fdae
Alexa <amzeazas@comcast.net>
2015-09-11 09:05:30 -0700

    Add form script.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,25 @@
-var replace = function(old, new) {
-  replace(old, new);
-};
+$(document).ready(function() {
+  $("form#sentence").submit(function(event) {
+    var text = $("textarea#text").val();
+
+    $(".output").empty();
+    $(".output").text(text)
+
+    $("#replace_form").show();
+    $("#result").show();
+
+    $("form#replace").submit(function(event) {
+      var old = $("input#old").val();
+      var replacement = $("input#replacement").val();
+      var newText = $(".output").text();
+      var result = newText.replace(old, replacement);
+
+      $(".output").text(result)
+
+      $("#result").show();
+      event.preventDefault();
+    });
+
+    event.preventDefault();
+  });
+});
